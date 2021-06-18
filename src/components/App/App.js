f403670a0fe9e71ad090ad00b3962508d18c069a
Olivia Finster <olivia.finster@gmail.com>
2021-06-18 16:37:27 -0500

    npm install, client side Routes, and formatted indents consistently with used files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,24 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
-        <Route path="/" exact>
-          <MovieList />
-        </Route>
-        
-        {/* Details page */}
-
-        {/* Add Movie page */}
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>The Movies Saga!</h1>
+            <Router>
+                <Route path="/" exact>
+                    <MovieList />
+                </Route>
+                <Route path="/details">
+                    {/* Details page */}
+                </Route>
+                <Route path="/add">
+                    {/* Add Movie page */}
+                </Route>
+            </Router>
+        </div>
+    );
 }
 
 
