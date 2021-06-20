30848874e5fcec80e50be138118247ea12ff8d04
Olivia Finster <olivia.finster@gmail.com>
2021-06-20 11:26:04 -0500

    more material ui installs and added theme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,22 +6,41 @@ import MovieList from '../MovieList/MovieList';
 import Details from '../Details/Details';
 import AddMovie from '../AddMovie/AddMovie';
 
+// material ui
+import '@fontsource/roboto';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/Styles';
+
+
 function App() {
+
+    const theme = createMuiTheme({
+        palette: {
+            primary: {
+                main: '#ffa4a2'
+            },
+            secondary: {
+                main: '#af4448'
+            }
+        }
+    })
+
     return (
-        <div className="App">
-            <h1>Movies</h1>
-            <Router>
-                <Route path="/" exact>
-                    <MovieList />
-                </Route>
-                <Route path="/details">
-                    <Details />
-                </Route>
-                <Route path="/add">
-                    <AddMovie />
-                </Route>
-            </Router>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className="App">
+                <h1>Movies</h1>
+                <Router>
+                    <Route path="/" exact>
+                        <MovieList />
+                    </Route>
+                    <Route path="/details">
+                        <Details />
+                    </Route>
+                    <Route path="/add">
+                        <AddMovie />
+                    </Route>
+                </Router>
+            </div>
+        </ThemeProvider>
     );
 }
 
